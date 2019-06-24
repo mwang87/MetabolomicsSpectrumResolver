eb1bb3f45a927ea3699ad081e3095370b16c76e7
Mingxun Wang <mwang87@gmail.com>
2019-06-24 22:55:54 +0200

    removing other dependencies

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apt-get update -y
 RUN apt-get install -y python3-pip python3-dev build-essential
 
 RUN pip3 install urllib3==1.23
-RUN pip3 install peewee
 RUN pip3 install flask
 RUN pip3 install requests
 RUN pip3 install requests-cache
--- app.py
+++ app.py
@@ -2,10 +2,8 @@
 import os
 
 from flask import Flask
-from peewee import SqliteDatabase
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(APP_ROOT, './database/database.db')
 
 class CustomFlask(Flask):
   jinja_options = Flask.jinja_options.copy()
@@ -20,7 +18,6 @@ class CustomFlask(Flask):
 
 app = CustomFlask(__name__)
 app.config.from_object(__name__)
-db = SqliteDatabase(app.config['DATABASE'], pragmas=[('journal_mode', 'wal')])
 
 app.config['UPLOAD_FOLDER'] = './tempuploads'
 
