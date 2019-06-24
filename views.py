b12769b4240a0b7947e4b5978cc40143652eb529
Mingxun Wang <mwang87@gmail.com>
2019-06-24 22:55:54 +0200

    removing other dependencies

--- views.py
+++ views.py
@@ -2,7 +2,6 @@
 from flask import abort, jsonify, render_template, request, redirect, url_for, send_file, make_response
 
 from app import app
-from models import *
 
 import os
 import csv
@@ -13,27 +12,6 @@ import requests
 @app.route('/', methods=['GET'])
 def renderhomepage():
     return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def validate():
-    request_file = request.files['file']
-
-    local_filename = os.path.join(app.config['UPLOAD_FOLDER'], str(uuid.uuid4()))
-    request_file.save(local_filename)
-
-
-    response_dict = {}
-    response_dict["stats"] = []
-    num_lines = sum(1 for line in open(local_filename))
-    response_dict["stats"].append({"type":"total_rows", "value":num_lines})
-
-    try:
-        os.remove(local_filename)
-    except:
-        print("Cannot Remove File")
-
-    return json.dumps(response_dict)
-
 
 @app.route('/heartbeat', methods=['GET'])
 def testapi():
