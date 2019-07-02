501ccbe3cca82a6b0634eb6e881504951497a416
Mingxun Wang <mwang87@gmail.com>
2019-07-02 12:48:27 -0700

    adding parsing file

---
+++ parsing.py
@@ -0,0 +1,15 @@
+def parse_gnps_peak_text(text):
+    lines = text.split('\n')
+    # first 8 lines are header
+    lines = lines[8:]
+    peaks = []
+    for line in lines:
+        tokens = line.split()
+        if len(tokens) == 0: # final line?
+            continue 
+        peaks.append((float(tokens[0]),float(tokens[1])))
+    peaks.sort(key = lambda x: x[0]) # make sure sorted by m/z
+    spectrum = {}
+    spectrum['peaks'] = peaks
+    spectrum['n_peaks'] = len(peaks)
+    return spectrum
