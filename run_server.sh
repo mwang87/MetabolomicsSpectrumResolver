cc92f6debf1989ba20e5df09c34f8d88fe25089b
Mingxun Wang <mwang87@gmail.com>
2019-11-12 12:43:33 -0800

    adding access logs to production system

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source activate usi
 
-gunicorn -w 4 -b 0.0.0.0:5000 --timeout 3600 main:app
+gunicorn -w 4 -b 0.0.0.0:5000 --timeout 3600 main:app --access-logfile /app/logs/access.log
