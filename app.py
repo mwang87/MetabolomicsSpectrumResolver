a70140f63c91ef94554f03f8c3ea83d02d8d362d
Wout <wout@bittremieux.be>
2019-11-13 10:53:11 -0800

    Reformat app

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-# app.py
+import logging
 import os
 
 from flask import Flask
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+
 class CustomFlask(Flask):
-  jinja_options = Flask.jinja_options.copy()
-  jinja_options.update(dict(
-    block_start_string='(%',
-    block_end_string='%)',
-    variable_start_string='((',
-    variable_end_string='))',
-    comment_start_string='(#',
-    comment_end_string='#)',
-  ))
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='(%',
+        block_end_string='%)',
+        variable_start_string='((',
+        variable_end_string='))',
+        comment_start_string='(#',
+        comment_end_string='#)'))
+
 
 app = CustomFlask(__name__)
 app.config.from_object(__name__)
 
 app.config['TEMPFOLDER'] = '/tmp'
-
 try:
     os.mkdir(app.config['TEMPFOLDER'])
-except:
-    print("Cannot Create", app.config['TEMPFOLDER'])
+except FileExistsError:
+    logging.WARNING(f'Cannot create {app.config["TEMPFOLDER"]}')
