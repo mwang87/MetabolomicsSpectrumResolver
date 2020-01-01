8ee5c95d1518bde8916cdf6615baffd2da1eebf2
Mingxun Wang <mwang87@gmail.com>
2020-01-01 13:10:06 -0800

    enabling handling local tests

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -1,21 +1,22 @@
 import requests
+import os
 from usi_test_cases import test_usi_list
-PRODUCTION_URL = "metabolomics-usi.ucsd.edu"
+PRODUCTION_URL = os.environ.get("SERVER_URL", "https://metabolomics-usi.ucsd.edu")
 
 def test_heartbeat():
-    url = f"https://{PRODUCTION_URL}/heartbeat"
+    url = f"{PRODUCTION_URL}/heartbeat"
     r = requests.get(url)
     r.raise_for_status()
 
 def test_usi_pages():
     for usi in test_usi_list:
-        url = f"https://{PRODUCTION_URL}/spectrum/?usi={usi}"
+        url = f"{PRODUCTION_URL}/spectrum/?usi={usi}"
         r = requests.get(url)
         r.raise_for_status()
 
 def test_error_page():
     error_usi = "mzspec:GNPSLIBRARY:CCMSLIB0000"
-    url = f"https://{PRODUCTION_URL}/spectrum/?usi={error_usi}"
+    url = f"{PRODUCTION_URL}/spectrum/?usi={error_usi}"
 
     r = requests.get(url)
     assert(r.status_code == 500)
