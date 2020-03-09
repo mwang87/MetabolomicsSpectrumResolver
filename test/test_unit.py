16f2048c305a2a8da8f111cbe9dd84a1cd3b10ce
Wout <wout@bittremieux.be>
2020-03-09 14:28:49 -0700

    Fix mirror plot unit test

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,17 +1,24 @@
 import sys
 sys.path.insert(0, "..")
 import parsing  # noqa: E402
+
+import views
 from usi_test_cases import test_usi_list
 
 
 def test_uri_parse():
     for usi in test_usi_list:
-        parsing.parse_usi(usi)  
+        parsing.parse_usi(usi)
+
 
 def test_render_mirror():
-    import views
-
     plotting_params = views.default_plotting_args
-    plotting_params["mz_min"] = 0
-    plotting_params["mz_max"] = 250
-    views._generate_mirror_figure('mzdata:MASSBANK:BSU00002', 'mzdata:MASSBANK:BSU00002', "png", **plotting_params)
+    plotting_params['mz_min'] = 0
+    plotting_params['mz_max'] = 250
+    plotting_params['max_intensity'] = plotting_params['max_intensity_labeled']
+    views._generate_mirror_figure(
+        'mzdata:MASSBANK:BSU00002', 'mzdata:MASSBANK:BSU00002', 'png',
+        **plotting_params)
+
+
+test_render_mirror()
