25703c18a42012e295fec9cfb41d5d1a9f584395
Wout <wout@bittremieux.be>
2020-05-18 20:24:42 -0700

    Fix integration tests

--- test-production-integration/test_integration.py
+++ test-production-integration/test_integration.py
@@ -1,12 +1,19 @@
-import requests
 import os
+import requests
+from urllib.parse import quote
+
 from usi_test_cases import test_usi_list
-PRODUCTION_URL = os.environ.get("SERVER_URL", "https://metabolomics-usi.ucsd.edu")
+
+
+PRODUCTION_URL = os.environ.get(
+    "SERVER_URL", "https://metabolomics-usi.ucsd.edu")
+
 
 def test_heartbeat():
     url = f"{PRODUCTION_URL}/heartbeat"
     r = requests.get(url)
     r.raise_for_status()
+
 
 def test_usi_pages():
     for usi in test_usi_list:
@@ -14,17 +21,20 @@ def test_usi_pages():
         r = requests.get(url)
         r.raise_for_status()
 
+
 def test_png_pages():
     for usi in test_usi_list:
         url = f"{PRODUCTION_URL}/png/?usi={usi}"
         r = requests.get(url)
         r.raise_for_status()
 
+
 def test_svg_pages():
     for usi in test_usi_list:
         url = f"{PRODUCTION_URL}/svg/?usi={usi}"
         r = requests.get(url)
         r.raise_for_status()
+
 
 def test_json():
     from urllib.parse import quote
@@ -44,11 +54,13 @@ def test_csv_pages():
         r = requests.get(url)
         r.raise_for_status()
 
+
 def test_qr_pages():
     for usi in test_usi_list:
         url = f"{PRODUCTION_URL}/qrcode/?usi={usi}"
         r = requests.get(url)
         r.raise_for_status()
+
 
 def test_error_page():
     error_usi = "mzspec:GNPSLIBRARY:CCMSLIB0000"
@@ -57,21 +69,27 @@ def test_error_page():
     r = requests.get(url)
     assert(r.status_code == 500)
 
+
 def test_mirror_img():
-    url = f"{PRODUCTION_URL}/mirror/?usi1=mzdata:MASSBANK:BSU00002&usi2=mzdata:MASSBANK:BSU00002"
+    url = (f"{PRODUCTION_URL}/mirror/?usi1=mzspec:MASSBANK::accession:BSU00002"
+           f"&usi2=mzspec:MASSBANK::accession:BSU00002")
     r = requests.get(url)
     r.raise_for_status()
 
-    url = f"{PRODUCTION_URL}/svg/mirror?usi1=mzdata:MASSBANK:BSU00002&usi2=mzdata:MASSBANK:BSU00002"
+    url = (f"{PRODUCTION_URL}/svg/mirror?"
+           f"usi1=mzspec:MASSBANK::accession:BSU00002&"
+           f"usi2=mzspec:MASSBANK::accession:BSU00002")
     r = requests.get(url)
     r.raise_for_status()
 
-    url = f"{PRODUCTION_URL}/png/mirror?usi1=mzdata:MASSBANK:BSU00002&usi2=mzdata:MASSBANK:BSU00002"
+    url = (f"{PRODUCTION_URL}/png/mirror?"
+           f"usi1=mzspec:MASSBANK::accession:BSU00002&"
+           f"usi2=mzspec:MASSBANK::accession:BSU00002")
     r = requests.get(url)
     r.raise_for_status()
+
 
 def test_img():
-    from urllib.parse import quote
     for usi in test_usi_list:
         url = f"{PRODUCTION_URL}/svg/?usi={quote(usi)}"
         r = requests.get(url)
