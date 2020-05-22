c56e6f127c7aa76bb98d0c108b23256a9dde4b16
Mingxun Wang <mwang87@gmail.com>
2020-05-22 16:21:10 -0700

    adding examples for MS1

--- test/usi_test_cases.py
+++ test/usi_test_cases.py
@@ -7,4 +7,5 @@ test_usi_list = ['mzspec:GNPSTASK-c95481f0c53d42e78a61bf899e9f9adb:spectra/specs
 'mzspec:GNPSLIBRARY:CCMSLIB00005436077',
 'mzspec:MOTIFDB:motif:171163',
 'mzspec:MS2LDATASK-190:document:270684',
-'mzspec:ST000003:iPSC-T1R1:scan:3']
+'mzspec:ST000003:iPSC-T1R1:scan:3',
+'mzspec:MSV000085444:Hui_N3_fe:scan:500']
