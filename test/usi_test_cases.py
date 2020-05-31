fdb8c41c7c5eabaaffa0e76c8082bd50856cb8e4
Mingxun Wang <mwang87@gmail.com>
2020-05-31 11:17:09 -0700

    bug fix for test cases

--- test/usi_test_cases.py
+++ test/usi_test_cases.py
@@ -1,5 +1,5 @@
 test_usi_list = ['mzspec:GNPSTASK-c95481f0c53d42e78a61bf899e9f9adb:spectra/specs_ms.mgf:scan:1943', # noqa: E501
-'mzspec:GNPSTASK-64b22841ab3548f987b3cfc18696a581:spectra/specs_ms.mgf:scan:1469'
+'mzspec:GNPSTASK-64b22841ab3548f987b3cfc18696a581:spectra/specs_ms.mgf:scan:1469',
 'mzspec:MSV000079514:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555',
 'mzspec:MTBLS38:(-)-epigallocatechin:scan:2',
 'mzspec:MSV000082791:(-)-epigallocatechin:scan:2',
