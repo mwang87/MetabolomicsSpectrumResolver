2f548542e785e0ba3fe4987cafa262447b53b845
Wout <wout@bittremieux.be>
2020-07-21 09:20:25 -0700

    Don't consume relevant exceptions by legacy parsing

--- parsing.py
+++ parsing.py
@@ -65,27 +65,26 @@ def _match_usi(usi):
 def parse_usi(usi):
     try:
         match = _match_usi(usi)
-        collection = match.group(1).lower()
-        # Send all proteomics USIs to MassIVE.
-        if (collection.startswith('msv') or
-                collection.startswith('pxd') or
-                collection.startswith('pxl') or
-                collection.startswith('rpxd') or
-                collection == 'massivekb'):
-            return _parse_msv_pxd(usi)
-        elif collection == 'gnps':
-            return _parse_gnps(usi)
-        elif collection == 'massbank':
-            return _parse_massbank(usi)
-        elif collection == 'ms2lda':
-            return _parse_ms2lda(usi)
-        elif collection == 'motifdb':
-            return _parse_motifdb(usi)
-        else:
-            raise ValueError(f'Unknown USI collection: {match.group(1)}')
     except ValueError:
-        # Lets try to parse the legacy.
         return parsing_legacy.parse_usi_legacy(usi)
+    collection = match.group(1).lower()
+    # Send all proteomics USIs to MassIVE.
+    if (collection.startswith('msv') or
+            collection.startswith('pxd') or
+            collection.startswith('pxl') or
+            collection.startswith('rpxd') or
+            collection == 'massivekb'):
+        return _parse_msv_pxd(usi)
+    elif collection == 'gnps':
+        return _parse_gnps(usi)
+    elif collection == 'massbank':
+        return _parse_massbank(usi)
+    elif collection == 'ms2lda':
+        return _parse_ms2lda(usi)
+    elif collection == 'motifdb':
+        return _parse_motifdb(usi)
+    else:
+        raise ValueError(f'Unknown USI collection: {match.group(1)}')
 
 
 # Parse GNPS tasks or library spectra.
