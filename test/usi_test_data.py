395499f202019960d73786c72af0208efadaea35
Wout <wout@bittremieux.be>
2020-09-02 14:57:01 -0700

    Include RPXD USI
    Doesn't seem to work on the MassIVE end?

--- test/usi_test_data.py
+++ test/usi_test_data.py
@@ -1,13 +1,15 @@
 usis_to_test = [
     # Real USIs.
-    'mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1943',
     'mzspec:MSV000079514:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555',
-    'mzspec:MSV000082791:(-)-epigallocatechin:scan:2',
     'mzspec:PXD000561:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555',
-    'mzspec:MASSBANK::accession:SM858102',
+    # FIXME: RPXD not resolved by MassIVE.
+    # 'mzspec:RPXD006668:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555',
+    'mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/specs_ms.mgf:scan:1943',
     'mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005436077',
-    'mzspec:MOTIFDB::accession:171163',
+    'mzspec:MASSBANK::accession:SM858102',
     'mzspec:MS2LDA:TASK-190:accession:270684',
+    'mzspec:MOTIFDB::accession:171163',
+    'mzspec:MSV000082791:(-)-epigallocatechin:scan:2',
     'mzspec:MSV000082680:iPSC-T1R1:scan:3',
     # Legacy cases.
     'mzspec:GNPSTASK-c95481f0c53d42e78a61bf899e9f9adb:spectra/specs_ms.mgf:scan:1943',
