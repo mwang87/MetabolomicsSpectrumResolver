812f6a113b1e43daa242021bba32253524351097
Wout <wout@bittremieux.be>
2020-09-02 16:42:05 -0700

    Avoid comparisons to None m/z min/max

--- views.py
+++ views.py
@@ -616,9 +616,9 @@ def _get_plotting_args(args: werkzeug.datastructures.ImmutableMultiDict,
     if plotting_args['height'] <= 0:
         plotting_args['height'] = default_plotting_args['height']
     # Make sure that the mass range is valid.
-    if plotting_args['mz_min'] <= 0:
+    if plotting_args['mz_min'] is not None and plotting_args['mz_min'] <= 0:
         del plotting_args['mz_min']
-    if plotting_args['mz_max'] <= 0:
+    if plotting_args['mz_max'] is not None and plotting_args['mz_max'] <= 0:
         del plotting_args['mz_max']
     # Set maximum intensity based on the plot type.
     plotting_args['max_intensity'] = _get_max_intensity(
