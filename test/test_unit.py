54409d16b8a41ea9e76e2a240e534b7d700fe2a2
Wout <wout@bittremieux.be>
2020-09-02 15:37:45 -0700

    Test USI parsing for various resources

--- test/test_unit.py
+++ test/test_unit.py
@@ -42,3 +42,92 @@ def test_parse_usi_invalid():
     with pytest.raises(ValueError):
         parsing.parse_usi('mzspec:PXD000561:'
                           'Adult_Frontalcortex_bRP_Elite_85_f09:scan:')
+
+
+def test_parse_gnps_task():
+    usi = ('mzspec:GNPS:TASK-c95481f0c53d42e78a61bf899e9f9adb-spectra/'
+           'specs_ms.mgf:scan:1943')
+    parsing.parse_usi(usi)
+    # Invalid task pattern.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':TASK-', ':TASK-666'))
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':scan:', ':index:'))
+    # Invalid file name.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace('specs_ms.mgf', 'nonexisting.mgf'))
+    # Invalid scan number.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':1943', ':this_scan_does_not_exist'))
+
+
+def test_parse_gnps_library():
+    usi = 'mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005436077'
+    parsing.parse_usi(usi)
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    # Invalid accession.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':CCMSLIB00005436077',
+                                      ':this_accession_does_not_exist'))
+
+
+def test_parse_massbank():
+    usi = 'mzspec:MASSBANK::accession:SM858102'
+    parsing.parse_usi(usi)
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    # Invalid accession.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':SM858102',
+                                      ':this_accession_does_not_exist'))
+
+
+def test_parse_ms2lda():
+    usi = 'mzspec:MS2LDA:TASK-190:accession:270684'
+    parsing.parse_usi(usi)
+    # Invalid task pattern.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':TASK-', ':TASK-bla'))
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    # Invalid experiment ID.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':TASK-190', ':TASK-666666666'))
+    # Invalid document ID.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':270684',
+                                      ':this_document_does_not_exist'))
+
+
+def test_parse_msv_pxd():
+    usi = 'mzspec:MSV000079514:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555'
+    parsing.parse_usi(usi)
+    # Invalid collection.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':MSV000079514:', ':MSV666666666:'))
+    # Invalid file name.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace('Adult_Frontalcortex_bRP_Elite_85_f09',
+                                      'this_filename_does_not_exist'))
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':scan:', ':index:'))
+    # Invalid scan number.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':17555', ':this_scan_does_not_exist'))
+
+
+def test_parse_motifdb():
+    usi = 'mzspec:MOTIFDB::accession:171163'
+    parsing.parse_usi(usi)
+    # Invalid index flag.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    # Invalid index.
+    with pytest.raises(ValueError):
+        parsing.parse_usi(usi.replace(':171163', ':this_index_does_not_exist'))
