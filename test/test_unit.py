557a9c5615d48c03816481f4c8ae8eef0b2fd805
Wout <wout@bittremieux.be>
2020-09-03 15:43:50 -0700

    Proper HTTP error codes
    Fixes #114.

--- test/test_unit.py
+++ test/test_unit.py
@@ -9,6 +9,7 @@ from spectrum_utils import spectrum as sus
 
 import parsing
 import views
+from error import UsiError
 
 from usi_test_data import usis_to_test
 
@@ -23,30 +24,36 @@ def test_parse_usi():
 
 
 def test_parse_usi_invalid():
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi('this:is:an:invalid:usi')
+    assert exc_info.value.error_code == 400
     # Invalid preamble.
     # FIXME: Exception not thrown because of legacy parsing.
-    # with pytest.raises(ValueError):
+    # with pytest.raises(UsiError) as exc_info:
     #     parsing.parse_usi('not_mzspec:PXD000561:'
     #                       'Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555')
+    # assert exc_info.value.error_code == 400
     # Invalid collection.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi('mzspec:PXD000000000:'
                           'Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555')
-    with pytest.raises(ValueError):
+    assert exc_info.value.error_code == 400
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi('mzspec:RANDOM666:'
                           'Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555')
+    assert exc_info.value.error_code == 400
     # Invalid index.
     # FIXME: Exception not thrown because of legacy parsing.
-    # with pytest.raises(ValueError):
+    # with pytest.raises(UsiError) as exc_info:
     #     parsing.parse_usi('mzspec:PXD000561:'
     #                       'Adult_Frontalcortex_bRP_Elite_85_f09:'
     #                       'not_scan:17555')
+    # assert exc_info.value.error_code == 400
     # Missing index.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi('mzspec:PXD000561:'
                           'Adult_Frontalcortex_bRP_Elite_85_f09:scan:')
+    assert exc_info.value.error_code == 400
 
 
 def test_parse_gnps_task():
@@ -54,88 +61,106 @@ def test_parse_gnps_task():
            'specs_ms.mgf:scan:1943')
     parsing.parse_usi(usi)
     # Invalid task pattern.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':TASK-', ':TASK-666'))
+    assert exc_info.value.error_code == 400
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':scan:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid file name.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace('specs_ms.mgf', 'nonexisting.mgf'))
+    assert exc_info.value.error_code == 404
     # Invalid scan number.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':1943', ':this_scan_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def test_parse_gnps_library():
     usi = 'mzspec:GNPS:GNPS-LIBRARY:accession:CCMSLIB00005436077'
     parsing.parse_usi(usi)
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid accession.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':CCMSLIB00005436077',
                                       ':this_accession_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def test_parse_massbank():
     usi = 'mzspec:MASSBANK::accession:SM858102'
     parsing.parse_usi(usi)
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid accession.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':SM858102',
                                       ':this_accession_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def test_parse_ms2lda():
     usi = 'mzspec:MS2LDA:TASK-190:accession:270684'
     parsing.parse_usi(usi)
     # Invalid task pattern.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':TASK-', ':TASK-bla'))
+    assert exc_info.value.error_code == 400
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid experiment ID.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':TASK-190', ':TASK-666666666'))
+    assert exc_info.value.error_code == 404
     # Invalid document ID.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':270684',
                                       ':this_document_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def test_parse_msv_pxd():
     usi = 'mzspec:MSV000079514:Adult_Frontalcortex_bRP_Elite_85_f09:scan:17555'
     parsing.parse_usi(usi)
     # Invalid collection.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':MSV000079514:', ':MSV666666666:'))
+    assert exc_info.value.error_code == 404
     # Invalid file name.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace('Adult_Frontalcortex_bRP_Elite_85_f09',
                                       'this_filename_does_not_exist'))
+    assert exc_info.value.error_code == 404
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':scan:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid scan number.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':17555', ':this_scan_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def test_parse_motifdb():
     usi = 'mzspec:MOTIFDB::accession:171163'
     parsing.parse_usi(usi)
     # Invalid index flag.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':accession:', ':index:'))
+    assert exc_info.value.error_code == 400
     # Invalid index.
-    with pytest.raises(ValueError):
+    with pytest.raises(UsiError) as exc_info:
         parsing.parse_usi(usi.replace(':171163', ':this_index_does_not_exist'))
+    assert exc_info.value.error_code == 404
 
 
 def _get_plotting_args(**kwargs):
