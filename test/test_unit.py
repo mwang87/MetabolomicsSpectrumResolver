4ae1437f154630454b3d0ead9e3990626d751413
Ming Wang <mwang87@gmail.com>
2020-09-03 14:36:34 -0700

    Revert "Revert "Optimize spectrum processing and peak annotation""

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,43 +1,40 @@
 import sys
-sys.path.insert(0, "..")
-import parsing  # noqa: E402
+sys.path.insert(0, '..')
 
+import parsing
 import views
-from usi_test_cases import test_usi_list
+
+import usi_test_cases
 
 
-def test_uri_parse():
-    for usi in test_usi_list:
-        print(f'TESTING USI {usi}')
+def test_usi_parse():
+    for usi in usi_test_cases.test_usi_list:
         parsing.parse_usi(usi)
 
 
 def test_render_mirror():
     plotting_params = views.default_plotting_args
-    plotting_params['mz_min'] = 0
-    plotting_params['mz_max'] = 250
+    plotting_params['mz_min'], plotting_params['mz_max'] = 0, 250
     plotting_params['max_intensity'] = plotting_params['max_intensity_labeled']
-    views._generate_mirror_figure(
-        'mzspec:MASSBANK::accession:BSU00002',
-        'mzspec:MASSBANK::accession:BSU00002',
-        'png', **plotting_params)
+    spectrum1, _ = parsing.parse_usi('mzspec:MASSBANK::accession:BSU00002')
+    spectrum2, _ = parsing.parse_usi('mzspec:MASSBANK::accession:BSU00002')
+    views._generate_mirror_figure(spectrum1, spectrum2, 'png',
+                                  **plotting_params)
 
 
 def test_render_single_plot():
     plotting_params = views.default_plotting_args
-    plotting_params['mz_min'] = 0
-    plotting_params['mz_max'] = 250
+    plotting_params['mz_min'], plotting_params['mz_max'] = 0, 250
     plotting_params['max_intensity'] = plotting_params['max_intensity_labeled']
-    views._generate_figure(
-        'mzspec:MASSBANK::accession:BSU00002', 'png', **plotting_params)
+    spectrum, _ = parsing.parse_usi('mzspec:MASSBANK::accession:BSU00002')
+    views._generate_figure(spectrum, 'png', **plotting_params)
 
 
 def test_render_single_plot_annotated():
     plotting_params = views.default_plotting_args
-    plotting_params['mz_min'] = 100
-    plotting_params['mz_max'] = 200
+    plotting_params['mz_min'], plotting_params['mz_max'] = 0, 200
     plotting_params['annotate_peaks'] = (
         [75.0225, 93.0575, 128.0275, 139.0075], [])
     plotting_params['max_intensity'] = plotting_params['max_intensity_labeled']
-    views._generate_figure(
-        'mzspec:MS2LDA:TASK-190:accession:270684', 'png', **plotting_params)
+    spectrum, _ = parsing.parse_usi('mzspec:MS2LDA:TASK-190:accession:270684')
+    views._generate_figure(spectrum, 'png', **plotting_params)
