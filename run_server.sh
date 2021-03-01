7179eaad381edf9ba3a730344d9a5f82a3ff7534
Mingxun Wang <mwang87@gmail.com>
2021-03-01 10:35:52 -0800

    updating configuration

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source activate usi
 
-gunicorn -w 2 -b 0.0.0.0:5000 --timeout 3600 main:app --access-logfile /app/logs/access.log --max-requests 1000
+gunicorn -w 2 --threads=1 -b 0.0.0.0:5000 --timeout 3600 main:app --access-logfile /app/logs/access.log --timeout 30 --max-requests 500 --max-requests-jitter 100
