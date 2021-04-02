d3d9947dc81e2657ca6de10262eb7b3822cec617
Mingxun Wang <mwang87@gmail.com>
2021-04-02 16:12:02 -0700

    supporting url encodes for potenially weird + and - symbols

--- test/usi_test_data.py
+++ test/usi_test_data.py
@@ -17,10 +17,13 @@ usis_to_test = [
     # Proteomics Annotations
     'mzspec:MSV000080846:2DRPRP_T42_20ug_Enol_FN08_360min_C2_110915.mzML:scan:1636:AAAPAPVSEAVC[+57.021]R/2',
     'mzspec:PXD000966:CPTAC_CompRef_00_iTRAQ_05_2Feb12_Cougar_11-10-09.mzML:scan:12298:[UNIMOD:214]-LHFFM[UNIMOD:35]PGFAPLTSR/2',
+    'mzspec:PXD015901:TCGA_BH-A18U_A2-A0YI_A2-A0EQ_117C_W_BI_20130405_H-JQ_f13.mzML:scan:28941:[+144.102063]-HPYFYAPELLFFAK[+144.102063]R/2',
     # MassIVE Task USIs
     'mzspec:MassIVE:TASK-f4b86b150a164ee4a440b661e97a7193-spectra:scan:471429:TSMGGTQQQFVEGVR/2',
     'mzspec:MassIVE:TASK-f4b86b150a164ee4a440b661e97a7193-spectra/specs_ms.mgf:scan:550734:LFIGGLSFETTDESLR/2',
     'mzspec:MassIVE:TASK-f4b86b150a164ee4a440b661e97a7193-spectra:scan:309566:AAAPAPVSEAVC[+57.021]R/2',
+    'mzspec:PXD002854:20150414_QEp1_LC7_GaPI_SA_Serum_LH_30.mzXML:scan:15073:HPYFYAPELLFFAKR/3',
+    'mzspec:MassIVE:TASK-f4b86b150a164ee4a440b661e97a7193-spectra/specs_ms.mgf:scan:287215:HPYFYAPELLF[-10.059]FAKR/3',
     # Legacy cases.
     'mzspec:GNPSTASK-c95481f0c53d42e78a61bf899e9f9adb:spectra/specs_ms.mgf:scan:1943',
     'mzspec:GNPSTASK-64b22841ab3548f987b3cfc18696a581:spectra/specs_ms.mgf:scan:1469',
