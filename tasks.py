6ba51dae82c86f34f907db7d7fe5c832d20b5d7e
Mingxun Wang <mwang87@gmail.com>
2021-04-10 13:11:12 -0700

    adding celery once

--- tasks.py
+++ tasks.py
@@ -1,7 +1,17 @@
 from celery import Celery
+from celery_once import QueueOnce
 import sys
 
-celery_instance = Celery('tasks', backend='redis://metabolomicsusi-redis', broker='pyamqp://guest@metabolomicsusi-rabbitmq//', )
+celery_instance = Celery('tasks', backend='redis://metabolomicsusi-redis', broker='pyamqp://guest@metabolomicsusi-rabbitmq//')
+celery.conf.ONCE = {
+  'backend': 'celery_once.backends.Redis',
+  'settings': {
+    'url': 'redis://metabolomicsusi-redis:6379/0',
+    'default_timeout': 60,
+    'blocking' : True,
+    'blocking_timeout' : 60
+  }
+}
 
 @celery_instance.task(time_limit=60)
 def task_computeheartbeat():
