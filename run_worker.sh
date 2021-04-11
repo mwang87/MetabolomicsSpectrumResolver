bf59ee7957680a9866af94b279051a3871214708
Mingxun Wang <mwang87@gmail.com>
2021-04-11 10:02:57 -0700

    adding security comment

--- run_worker.sh
+++ run_worker.sh
@@ -2,4 +2,5 @@
 source activate usi
 
 export C_FORCE_ROOT="true"
+#TODO: Make sure we don't run this worker as root
 celery -A tasks worker -l info -c 1 -Q worker --max-tasks-per-child 10 --loglevel INFO
