eb3d246722fba738cc8a978cc8e64e11f75c5e82
Wout <wout@bittremieux.be>
2021-08-18 12:11:27 -0700

    Remove mzdraft replacing

--- parsing.py
+++ parsing.py
@@ -386,8 +386,6 @@ def _parse_ms2lda(usi: str) -> Tuple[sus.MsmsSpectrum, str]:
 
 # Parse MSV or PXD library.
 def _parse_msv_pxd(usi: str) -> Tuple[sus.MsmsSpectrum, str]:
-    if usi.startswith("mzdraft:"):
-        usi = usi.replace("mzdraft:", "mzspec:")
     match = _match_usi(usi)
     dataset_identifier = match.group(1)
     index_flag = match.group(3)
