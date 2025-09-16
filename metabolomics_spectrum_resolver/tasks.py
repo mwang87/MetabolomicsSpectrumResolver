40ebe237e7540cb14755eb68dcbb075f071d267b
Mingxun Wang <mwang87@gmail.com>
2025-09-16 11:07:26 -0700

    bug fix

--- metabolomics_spectrum_resolver/tasks.py
+++ metabolomics_spectrum_resolver/tasks.py
@@ -118,7 +118,7 @@ def parse_usi(usi: str) -> Tuple[sus.MsmsSpectrum, str, str]:
 
     # Debugging logging
     import sys
-    sys.stderr.write("Parsing {}\n".format(usi), file=sys.stderr, flush=True)
+    print("Parsing {}\n".format(usi), file=sys.stderr, flush=True)
 
     # First attempt to schedule with Celery.
     try:
