15269ad200708617d8dcf1a96eb656ee9c90fe40
Mingxun Wang <mwang87@gmail.com>
2025-09-16 11:04:04 -0700

    updating workers count

--- run_worker.sh
+++ run_worker.sh
@@ -7,4 +7,4 @@ export C_FORCE_ROOT="true"
 celery -A metabolomics_spectrum_resolver.tasks_analytics worker --concurrency=1 -Q worker-analytics --loglevel INFO --detach
 
 #TODO: Make sure we don't run this worker as root
-celery -A metabolomics_spectrum_resolver.tasks worker -l info --autoscale=16,1 -Q worker --max-tasks-per-child 10 --loglevel INFO
+celery -A metabolomics_spectrum_resolver.tasks worker -l info --autoscale=32,1 -Q worker --max-tasks-per-child 10 --loglevel INFO
