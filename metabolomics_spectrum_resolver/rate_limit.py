1ba34f59bbc9fc3e2ba74ab8b3e7184dfc406366
mwang87 <mwang87@gmail.com>
2026-03-31 15:17:07 -0700

    adding missing file

---
+++ metabolomics_spectrum_resolver/rate_limit.py
@@ -0,0 +1,38 @@
+import ipaddress
+
+import flask
+from flask_limiter import Limiter
+
+WHITELISTED_RANGES = [
+    ipaddress.ip_network("138.23.0.0/16"),    # UCR
+    ipaddress.ip_network("169.235.0.0/16"),    # UCR
+    ipaddress.ip_network("132.239.0.0/16"),    # UCSD
+    ipaddress.ip_network("137.110.0.0/16"),    # UCSD
+    ipaddress.ip_network("192.31.146.0/24"),   # UCSD
+]
+
+
+def get_ip():
+    if flask.request.headers.getlist("X-Forwarded-For"):
+        ip = flask.request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        ip = flask.request.remote_addr
+    return ip.split(",")[0].strip()
+
+
+def get_ip_or_exempt():
+    try:
+        client_ip = ipaddress.ip_address(get_ip())
+        for network in WHITELISTED_RANGES:
+            if client_ip in network:
+                return "whitelisted-user"
+    except ValueError:
+        pass
+    return get_ip()
+
+
+limiter = Limiter(
+    key_func=get_ip_or_exempt,
+    default_limits=[],
+    storage_uri="redis://metabolomicsusi-redis:6379",
+)
